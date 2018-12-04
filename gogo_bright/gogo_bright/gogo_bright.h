3394661b7befb62eca2579ef6202271a350200c4
MOMELAPTOP\peera <peeranut.31@hotmail.com>
2018-12-04 16:39:11 +0700

    edit to support GoGo Bright 1.0 protocol .. no longer support older fw

--- gogo_bright/gogo_bright/gogo_bright.h
+++ gogo_bright/gogo_bright/gogo_bright.h
@@ -8,7 +8,7 @@ using std::string;
 #include "device.h"
 #include "i2c-dev.h"
 
-#define GOGO_BRIGHT_POLLING_MS					40
+#define GOGO_BRIGHT_POLLING_MS 40
 
 #define GOGOBRIGHT_I2C_ADDRESS 0x42
 #define GOGOBRIGHT_ID_1 0x60
@@ -27,79 +27,89 @@ using std::string;
 
 //* ///////////////////////////////////
 //* Commands I2C
+#define CATEGORY_CMD 0
 //* servo
-#define CMD_SERVO_ACTIVE 91
-#define CMD_SERVO_SETH 87
-#define CMD_SERVO_CCW 88
-#define CMD_SERVO_CW 89
+#define CMD_SERVO_ACTIVE 14
+#define CMD_SERVO_SETH 9
+#define CMD_SERVO_CCW 15 // aka Thatway
+#define CMD_SERVO_CW 16  // aka Thisway
 //* motor
-#define CMD_MOTOR_ACTIVE 90
-#define CMD_MOTOR_ON 49
-#define CMD_MOTOR_OFF 51
-#define CMD_MOTOR_CCW 52
-#define CMD_MOTOR_CW 53
-#define CMD_MOTOR_RD 54
-#define CMD_MOTOR_PWR 59
+#define CMD_MOTOR_ACTIVE 7
+#define CMD_MOTOR_ONOFF 2
+// #define CMD_MOTOR_OFF 51
+#define CMD_MOTOR_DIR 3
+// #define CMD_MOTOR_CW 53
+#define CMD_MOTOR_RD 4
+#define CMD_MOTOR_PWR 6
 //* i2c
 #define CMD_I2C_WRITE 107
 #define CMD_I2C_READ 108
 
-class GOGO_BRIGHT : public Device {
-	private:
-		enum {
-			 s_detect, s_get_sensor, s_error, s_wait
-		} state;
-		TickType_t tickcnt, polling_tickcnt;
-		bool first_read;
-		double illuminance;
-		double calc_lux(unsigned int ch0, unsigned int ch1);
+class GOGO_BRIGHT : public Device
+{
+  private:
+	enum
+	{
+		s_detect,
+		s_get_sensor,
+		s_error,
+		s_wait
+	} state;
 
-		I2CDev *i2c;
-		int sensor_values[4];
+	TickType_t tickcnt, polling_tickcnt;
+	bool first_read;
+	bool i2c_bus_flag = 0;
+	// double illuminance;
+	// double calc_lux(unsigned int ch0, unsigned int ch1);
 
-		bool wireWriteDataByte(uint8_t reg, uint8_t val);
+	I2CDev *i2c;
+	int sensor_values[4];
 
-	public:
-		// constructor
-		GOGO_BRIGHT();
-		// override
-		void init(void);
-		void process(Driver *drv);
-		int prop_count(void);
-		bool prop_name(int index, char *name);
-		bool prop_unit(int index, char *unit);
-		bool prop_attr(int index, char *attr);
-		bool prop_read(int index, char *value);
-		bool prop_write(int index, char *value);
-		// method
-		double get_illuminance(void);
+	bool wireWriteDataByte(uint8_t reg, uint8_t val);
+	bool wireWriteDataByte(uint8_t reg, uint8_t target, uint8_t val);
+	bool wireWriteDataByte(uint8_t reg, uint8_t target, uint8_t high_val, unit8_t low_val);
 
-		//* ********************* Input port functions *********************
-		//? get sensors value from input port 1-4
-		int readInput(int port);
-		
-		//* ********************* Motor functions *********************
-		//? set output to interact with ..
-		bool talkToOutput(string output_port);
-		// //? set output power
-		bool setOutputPower(int power);
-		// //? turn outputs on or off
-		bool turnOutputON(void);
-		bool turnOutputOFF(void);
-		// //? turn outputs direction
-		bool turnOutputThisWay(void);
-		bool turnOutputThatWay(void);
-		bool toggleOutputWay(void);
+  public:
+	// constructor
+	GOGO_BRIGHT();
+	// override
+	void init(void);
+	void process(Driver *drv);
+	int prop_count(void);
+	bool prop_name(int index, char *name);
+	bool prop_unit(int index, char *unit);
+	bool prop_attr(int index, char *attr);
+	bool prop_read(int index, char *value);
+	bool prop_write(int index, char *value);
+	// method
+	double get_illuminance(void);
 
-		//* ********************* Servo functions *********************
-		//? set servos to interact with ..
-		bool talkToServo(string servo_port);
-		//? set servos head to input head_angle
-		bool setServoHead(int head_angle);
-		//? turn servos clockwise by input angle
-		bool turnServoCW(int cw_angle);
-		//? turn servos counter-clockwise by input angle
-		bool turnServoCCW(int ccw_angle);
+	//* ********************* Input port functions *********************
+	//? get sensors value from input port 1-4
+	int readInput(int port);
+
+	//* ********************* Motor functions *********************
+	//? set output to interact with ..
+	bool talkToOutput(string output_port);
+	// //? set output power
+	bool setOutputPower(int power);
+	// //? turn outputs on or off
+	bool turnOutputON(void);
+	bool turnOutputOFF(void);
+	// //? turn outputs direction
+	bool turnOutputThisWay(void);
+	bool turnOutputThatWay(void);
+	bool toggleOutputWay(void);
+
+	//* ********************* Servo functions *********************
+	//? set servos to interact with ..
+	bool talkToServo(string servo_port);
+	//? set servos head to input head_angle
+	bool setServoHead(int head_angle);
+	//? turn servos clockwise by input angle
+	bool turnServoCW(int cw_angle);
+	//? turn servos counter-clockwise by input angle
+	bool turnServoCCW(int ccw_angle);
 };
 
 #endif
